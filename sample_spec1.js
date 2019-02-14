8ef653b52b9a14d672b2592185cbcb4f8a1531ae
Shreyas R <shrer@microsoft.com>
2019-02-14 17:01:45 +0530

    Initial commit

---
+++ sample_spec1.js
@@ -0,0 +1,76 @@
+describe('first set', () => {
+
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
+    beforeAll(() => {
+        console.log("before all.");
+    })
+
+    beforeEach(() => {
+        console.log("before each.");
+    })
+
+    it('Test method 1', () => {
+        pending();
+        expect(true).toBe(true);
+    });
+
+    it('Test method 2', () => {
+        pending();
+        expect(true).toBe(false);
+    });
+
+    it('Test method 3', () => {
+        console.log("bleh belh \n bleh bleh")
+    });
+
+    it('Test method 4', () => {
+        console.error("this is a custom error")
+        expect(true).toBe(false);
+    });
+
+    it('Test method 5', () => {
+        expect(true).toBe(true);
+    });
+
+    afterEach(() => {
+        //do something
+    })
+
+    afterAll(() => {
+        //do something
+    })
+})
+
+describe('second set', () => {
+    beforeAll(() => {
+
+    })
+
+    beforeEach(() => {
+        //do something
+    })
+
+    it('Test method 1', () => {
+        expect(true).toBe(true);
+    });
+
+    it('Test method 2', () => {
+        expect(true).toBe(false);
+    });
+
+    it('Test method 3', () => {
+        expect(true).toBe(true);
+    });
+
+    it('Test method 4', () => {
+        expect(true).toBe(false);
+    });
+
+    afterEach(() => {
+        //do something
+    })
+
+    afterAll(() => {
+        //expect(true).toBe(false);
+    })
+})
